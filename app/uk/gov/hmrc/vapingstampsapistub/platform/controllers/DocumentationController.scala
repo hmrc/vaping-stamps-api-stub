914368f7d0ba6144d7f41a535113bbeb989cb1d9
hmrc-web-operations <hmrc-web-operations@digital.hmrc.gov.uk>
2025-12-30 12:40:56 +0000

    'Creating new service vaping-stamps-api-stub'

---
+++ app/uk/gov/hmrc/vapingstampsapistub/platform/controllers/DocumentationController.scala
@@ -0,0 +1,19 @@
+package uk.gov.hmrc.platform.controllers
+
+import play.api.mvc.{Action, AnyContent, ControllerComponents}
+import controllers.Assets
+import uk.gov.hmrc.play.bootstrap.backend.controller.BackendController
+
+import javax.inject.{Inject, Singleton}
+
+@Singleton
+class DocumentationController @Inject() (
+  assets: Assets,
+  cc: ControllerComponents
+) extends BackendController(cc):
+
+  def definition(): Action[AnyContent] =
+    assets.at("/public/api", "definition.json")
+
+  def specification(version: String, file: String): Action[AnyContent] =
+    assets.at(s"/public/api/conf/$version", file)
