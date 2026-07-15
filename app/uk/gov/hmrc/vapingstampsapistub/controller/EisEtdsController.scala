f82f8bc23913b4889ce60a845d234810055b1a60
JSimmons93 <53817037+JSimmons072019@users.noreply.github.com>
2026-07-15 14:58:31 +0100

    Update

--- app/uk/gov/hmrc/vapingstampsapistub/controller/EisEtdsController.scala
+++ app/uk/gov/hmrc/vapingstampsapistub/controller/EisEtdsController.scala
@@ -61,10 +61,10 @@ class EisEtdsController @Inject() (
       case stamp if stamp.matches(stampsReferenceNumberRegex) =>
         val splitRefTuple = stamp.toSeq match {
           case Seq(c1, c2, c3, c4, c5, c6, c7, c8, c9, c10, c11, c12, c13) =>
-            (""+c1 + c2, c3, c4, "" + c5 + c6 + c7 + c8 + c9 + c10 + c11 + c12 + c13)
+            ("" + c1 + c2, c3, c4, "" + c5 + c6 + c7 + c8 + c9 + c10 + c11 + c12 + c13)
           case _ => throw new IllegalArgumentException("Reference does not match expected character length")
         }
-        return splitRefTuple
+        splitRefTuple
       case _ => throw new IllegalArgumentException("Stamp Reference number validation failed")
     }
   }
